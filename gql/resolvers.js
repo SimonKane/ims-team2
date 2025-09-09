6aa8bc74e7d423b4274b1a7da9a0d61a99c38c90
Moa Sjögren <moasjgren@gmail.com>
2025-09-09 16:19:00 +0200

    added get and post endpoints

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -1,1 +1,3 @@
 // resolvers
+
+// om contact inte finns på ett objekt, vad händer när man gör en query och enbart vill ha contact som retur
--- rest/routes/productRoutes.js
+++ rest/routes/productRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { createProduct } from "../controllers/productController.js";
+import { getAllProducts } from "../controllers/productController.js";
+import { getProductById } from "../controllers/productController.js";
+import { getAllManufacturers } from "../controllers/productController.js";
 
 const router = express.Router();
 
+router.get("/manufacturers", getAllManufacturers);
+router.get("/:id", getProductById);
+router.get("/", getAllProducts);
 router.post("/create", createProduct);
 export default router;
