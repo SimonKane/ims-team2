c43dde5119b28671c3ea8509ee8838b350b593bf
Moa Sjögren <moasjgren@gmail.com>
2025-09-09 15:14:00 +0200

    updated controllers and routes

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,38 @@
 // model
 import mongoose from "mongoose";
-import { Manufacturer } from "./Manufacturer";
 
-const product = new mongoose.Schema(
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+    phone: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const manufacturerSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    country: { type: String, required: true, trim: true },
+    website: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    address: { type: String, required: true, trim: true },
+    contact: contactSchema,
+  },
+  { _id: false }
+);
+
+const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     sku: { type: String, unique: true, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     price: { type: Number, required: true, min: 0 },
     category: { type: String, required: true, trim: true },
-    manufacturer: Manufacturer,
+    manufacturer: { type: manufacturerSchema, required: true },
     amountInStock: { type: Number, required: true, min: 0 },
   },
   { timestamps: true, collection: "products" }
 );
 
-export const Product = mongoose.model("Product", product);
+export const Product = mongoose.model("Product", productSchema);
--- rest/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/create", createContact);
-
-export default router;
